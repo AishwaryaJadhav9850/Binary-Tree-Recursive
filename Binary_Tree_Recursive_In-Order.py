cc12083bf321652faa339c00eed24794810f3311
AishwaryaJadhav9850 <39164674+AishwaryaJadhav9850@users.noreply.github.com>
2020-11-18 22:05:02 +0530

    Binary Tree In order

---
+++ Binary_Tree_Recursive_In-Order.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 22 20:47:33 2020
+
+@author: Admin
+"""
+
+"""
+Regular Binary Tree Implementation with Recursive In-Order Depth First Traversal
+
+"""
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.right=self.left=None
+    
+def insert(root,data):
+    if(root==None):        
+        root=Node(data)
+    print("Enter Left Child for ",root.data,":- ")
+    l=input()
+    if(l.isdigit()):
+        root.left=insert(root.left,int(l))  
+    print("Enter Right Child for ",root.data,":- ")
+    r=input()
+    if(r.isdigit()):
+        root.right=insert(root.right,int(r)) 
+    return root
+
+             
+    
+def display(root):   
+    if root.left !=None:
+        display(root.left)
+    print(root.data, end=" ")
+    if root.right !=None:
+        display(root.right)
+  
+root=None
+i=int(input("Enter data of the node:- "))
+root=insert(root,i)
+print("Tree Nodes are :")
+display(root)
